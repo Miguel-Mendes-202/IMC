7d7cfad7b18424eac1c153371f6fd47f28ef4b2b
Miguel Mendes <mendesmiguel2007@gmail.com>
2026-04-17 22:49:02 -0300

    Arrumar Proxima semana(17\04)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿double Peso, Altura, imc;
+
+Console.WriteLine("Digite seu Peso(KG)");
+Peso = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Agora sua Altura(m)");
+Altura = Convert.ToDouble(Console.ReadLine());
+
+imc= Peso/Math.Pow ("Altura, 2");
+
+
+Console.WriteLine($"\nIMC: {imc:N1} kg/m²");
